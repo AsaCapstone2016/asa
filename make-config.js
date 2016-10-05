214cfcd7b5dd05d765ab6bdb0d9a09ba86bfed0a
cse498 <cse498@cse498-0501-amazon.cse.msu.edu>
2016-10-05 14:45:36 -0400

    no block-scoped vars. REMEMBER TO mv config.js config.json !!!! .gitignore is changed to ignore config.json

--- make-config.js
+++ make-config.js
@@ -1,15 +1,17 @@
-let jsonfile = require('jsonfile');
+var jsonfile = require('jsonfile');
 
-const flag = process.argv[2];
-const FB_PAGE_TOKEN = (flag === '--prod') ? process.env.FB_PAGE_TOKEN_PROD
-                                          : process.env.FB_PAGE_TOKEN_DEV;
-const config = {
+var flag = process.argv[2];
+var FB_PAGE_TOKEN = (flag === '--prod') ? process.env.FB_PAGE_TOKEN_PROD
+                                        : process.env.FB_PAGE_TOKEN_DEV;
+var config = {
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
   AWS_ID : process.env.AWS_ID,
   AWS_SECRET: process.env.AWS_SECRET,
   WIT_TOKEN: process.env.WIT_TOKEN,
   FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN
 };
+
+console.log("CONFIG: ", config);
 
 jsonfile.writeFile('./config.json', config, (err) => {
   console.log('***ERROR WRITING CONFIG: ', error);
