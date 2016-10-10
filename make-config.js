4dd0d1208d0ee9a48034af588495ffcf5e38efd7
Aaron Beckett <aminor65ii@gmail.com>
2016-10-10 14:15:37 -0400

    Add Sessions table and db permissions to serverless.yml

--- make-config.js
+++ make-config.js
@@ -3,12 +3,15 @@ var jsonfile = require('jsonfile');
 var flag = process.argv[2];
 var FB_PAGE_TOKEN = (flag === '--prod') ? process.env.FB_PAGE_TOKEN_PROD
                                         : process.env.FB_PAGE_TOKEN_DEV;
+var TABLE_PREFIX  = (flag === '--prod') ? `ASA-prod-`
+                                        : `ASA-dev-`;
 var config = {
   FB_PAGE_TOKEN: FB_PAGE_TOKEN,
   AWS_ID : process.env.AWS_ID,
   AWS_SECRET: process.env.AWS_SECRET,
   WIT_TOKEN: process.env.WIT_TOKEN,
-  FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN
+  FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+  TABLE_PREFIX: TABLE_PREFIX
 };
 
 console.log("CONFIG: ", config);
