e486056a9a81628892dac155f55e4dc26f7f52b1
Evan Moran <moraneva@msu.edu>
2016-10-11 15:42:30 -0400

    Trying to get database stuff integrated with dev

---
+++ core/database/lib/purchasedItemDAO.js
@@ -0,0 +1,43 @@
+/**
+ * Created by cse498 on 10/11/16.
+ */
+'use strict';
+
+var AWS = require("aws-sdk");
+var config = require('./../../../config');
+
+AWS.config.update({
+    region: "us-east-1",
+    endpoint: "dynamodb.us-east-1.amazonaws.com"
+});
+
+const documentClient = new AWS.DynamoDB.DocumentClient();
+const tableName = `${config.TABLE_PREFIX}PurchasedItems`;
+
+var purchasedItemDAO = {
+    addItem: function (uid, ASIN) {
+        let date = new Date();
+
+        let params = {
+            TableName: tableName,
+            Item: {
+                "uid": uid,
+                "date": date.toISOString(),
+                "item": ASIN
+
+            }
+        };
+
+        return new Promise(function (resolve, reject) {
+            documentClient.put(params, function (err, data) {
+                if (err) {
+                    console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+                }
+                resolve();
+            });
+        });
+
+    }
+};
+
+module.exports = purchasedItemDAO;
--- make-config.js
+++ make-config.js
@@ -1,22 +1,24 @@
 var jsonfile = require('jsonfile');
 
 var flag = process.argv[2];
-var FB_PAGE_TOKEN = (flag === '--prod') ? process.env.FB_PAGE_TOKEN_PROD
-                                        : process.env.FB_PAGE_TOKEN_DEV;
-var TABLE_PREFIX  = (flag === '--prod') ? `ASA-prod-`
-                                        : `ASA-dev-`;
+var inProd = flag === '--prod';
+var FB_PAGE_TOKEN = inProd ? process.env.FB_PAGE_TOKEN_PROD
+    : process.env.FB_PAGE_TOKEN_DEV;
+var TABLE_PREFIX = inProd ? `ASA-prod-`
+    : `ASA-dev-`;
 var config = {
-  FB_PAGE_TOKEN: FB_PAGE_TOKEN,
-  AWS_ID : process.env.AWS_ID,
-  AWS_SECRET: process.env.AWS_SECRET,
-  WIT_TOKEN: process.env.WIT_TOKEN,
-  FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
-  TABLE_PREFIX: TABLE_PREFIX
+    FB_PAGE_TOKEN: FB_PAGE_TOKEN,
+    AWS_ID: process.env.AWS_ID,
+    AWS_SECRET: process.env.AWS_SECRET,
+    WIT_TOKEN: process.env.WIT_TOKEN,
+    FB_VERIFY_TOKEN: process.env.FB_VERIFY_TOKEN,
+    TABLE_PREFIX: TABLE_PREFIX,
+    IN_PROD: inProd
 };
 
 console.log("CONFIG: ", config);
 
 jsonfile.writeFile('./config.json', config, (err) => {
-  if (err)
-    console.log('***ERROR WRITING CONFIG: ', err);
+    if (err)
+        console.log('***ERROR WRITING CONFIG: ', err);
 });
