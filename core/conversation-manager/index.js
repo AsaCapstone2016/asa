756cb177c8e88249b26835d5304d271e43c619a1
Aaron Beckett <aminor65ii@gmail.com>
2016-10-11 13:02:53 -0400

    Change dashes to bullets in help message

--- core/conversation-manager/index.js
+++ core/conversation-manager/index.js
@@ -137,8 +137,8 @@ const actions = {
           let msg = "Think of me as your personal shopping assistant.";
           msg += " I can help you discover and purchase items on Amazon.";
           msg += " Try saying...\n\n";
-          msg += " - I want to buy something\n";
-          msg += " - Can you find Ocarina of Time?";
+          msg += " • I want to buy something\n";
+          msg += " • Can you find Ocarina of Time?";
           console.log(`SEND help message to ${recipientId}`);
           return messageSender.sendTextMessage(recipientId, msg)
             .then(() => {
