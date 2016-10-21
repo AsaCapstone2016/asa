1adfe063066918d9a52f7fdc6a92d25114c78e1f
Sam Chung <tieoneease@gmail.com>
2016-10-20 21:17:27 -0400

    requests script

--- batch-requests.js
+++ batch-requests.js
@@ -1,8 +1,5 @@
 var amazon_api = require("amazon-product-api");
 var config = require('./config');
-var fs = require('fs');
-var parse = require('csv-parse');
- 
 
 var amazon_client = amazon_api.createClient({
   awsId: config.AWS_ID,
@@ -15,21 +12,33 @@ function prettyPrint(json) {
   return JSON.stringify(json, null, 2);
 }
 
+
 function getItem(ASIN) {
-  return new amazon_client.itemLookup({
+  return amazon_client.itemLookup({
     "ItemId": ASIN,
     "IdType": "ASIN",
     "ResponseGroup": ["ItemAttributes","BrowseNodes"]
   }).then(res => {
-    let item = res[0];
-    return item;
+    if (res) {
+      let item = res[0];
+      return item;
+    }
   }).catch(err => {
+    console.log(err.Error[0]);
     if (err.Error[0].Code[0] === 'RequestThrottled') {
       setTimeout(() => {
         return getItem(ASIN);
-      }, 2000);
+      }, 3000);
     }
   });
+}
+
+function getItemsBatched(ASINs) {
+  return amazon_client.itemLookup({
+    "ItemId": ASINs,
+    "IdType": "ASIN",
+    "ResponseGroup": ["ItemAttributes","BrowseNodes"]
+  })
 }
 
 function getTitle(product) {
@@ -45,57 +54,43 @@ function getProductGroup(product) {
 }
 let wait = time => new Promise(f => setTimeout(f, time));
 
-let ASIN = '193223604X';
 let ASINs = [ 
   'B000L2DSLK',
-  '132354187',
   '193223604X',
-  '691089825',
-  '495107786',
-  '840033559',
-  '929408233',
   'B00008URUS',
-  '486250644',
-  '872206556',
-  '872200906',
   '091514560X',
-  '140446044',
-  '486250644',
-  '713990724',
   'B00BUIG73U',
   'B0094092XI',
-  '130159204',
-  '201633612',
   '053849882X',
   'B00E9I1FPI',
-  '739019333',
-  '739007459',
-  '739018655',
   'B00IQS8E5Q',
-  '123742609',
   '1118102274',
-  '136042597',
   'B00CY9RQ2K',
-  '133407152',
-  '131873253',
   'B00O9GW8TC',
-  'B01IM96BQM' ];
+  'B01IM96BQM'];
 
-let promises = [];
-for (let i = 0; i < 80; i++) {
-  //promises.push(getItem(ASIN));
+function getBatches(ASINs) {
+  let results = [];
+  for (let i = 0; i < ASINs.length; i += 10) {
+    let end = (i + 10 > ASINs.length) ? ASINs.length : i + 10;
+    results.push(ASINs.slice(i, end).join());
+  }
+  return results;
 }
 
-let items = [];
-Promise.all(promises).then(values => {
-  values.forEach(value => {
-    if (value)
-      items.push(value);
-  });
-  let i = 0;
-  items.forEach(item => {
-    console.log(i, getTitle(item));
-    i++;
-  });
-})
+let promises = [];
+getBatches(ASINs).forEach(query => {
+  promises.push(getItemsBatched(query));
+});
 
+let items = [];
+Promise.all(promises).then(responses => {
+  responses.forEach(response => {
+    response.forEach(item => {
+      items.push(getTitle(item));
+    })
+  })
+  console.log(items);
+}).catch(err => {
+  console.log('error: ', prettyPrint(err))
+})
