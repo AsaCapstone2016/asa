d185b33843c8cabc83992439a69eb20b67aed90c
Aaron Beckett <aminor65ii@gmail.com>
2016-10-25 21:07:06 -0400

    Treat simple quick replies as text actions
    Simple quick replies are quick replies without a special payload... aka their payload is the same as their title.

--- core/facebook-event-converter/index.js
+++ core/facebook-event-converter/index.js
@@ -26,36 +26,42 @@ var facebookEventConverter = {
 
             var entry_id = entry.id;
 
-            //Messaging is an array of messaging objects so we need to make sure to hit all of them.
+            // Messaging is an array of messaging objects so we need to make sure to hit all of them.
             entry.messaging.forEach(function (messaging) {
-                if (messaging.sender.id == entry_id)
+
+                const senderId = messaging.sender.id;
+                const message = messaging.message;
+                const postback = messaging.postback;
+
+                if (senderId == entry_id)
                     return;
-                //If message is part of this object, we are processing simple text message
-                if (messaging.message) {
-                    let msg_content;
-                    if (messaging.message.quick_reply !== undefined) {
-                        msg_content = {
-                            payload: messaging.message.quick_reply.payload,
-                            action: "postback"
-                        };
-                    } else {
-                        msg_content = {
-                            payload: messaging.message.text,
-                            action: "text"
-                        };
+
+                // If message is part of this object, we are processing simple text message
+                if (message) {
+                    let payload = message.text;
+                    let action = "text";
+                    
+                    // If this message has a quick reply check if there's a unique payload
+                    if (message.quick_reply !== undefined) {
+                        payload = message.quick_reply.payload;
+                        action = message.text == payload ? "text" : "postback";
                     }
+
                     messageObjects.push({
-                        UID: messaging.sender.id,
-                        content: msg_content
+                        UID: senderId,
+                        content: {
+                            payload: payload,
+                            action: action
+                        }
                     });
                 }
 
-                //If postback is part of this object, then we are processing postback message
-                else if (messaging.postback) {
+                // If postback is part of this object, then we are processing postback message
+                else if (postback) {
                     messageObjects.push({
-                        UID: messaging.sender.id,
+                        UID: senderId,
                         content: {
-                            payload: messaging.postback.payload,
+                            payload: postback.payload,
                             action: "postback"
                         }
                     });
