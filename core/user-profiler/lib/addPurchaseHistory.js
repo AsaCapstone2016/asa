632b9fead90a4c632399141692c4756e36465187
Aaron Beckett <aminor65ii@gmail.com>
2016-11-04 14:17:44 -0400

    Use the user profiles DAO in addPurchaseEvent

--- core/user-profiler/lib/addPurchaseHistory.js
+++ core/user-profiler/lib/addPurchaseHistory.js
@@ -1,6 +1,7 @@
 'use strict';
 
 let utils = require('./utils');
+let userProfilesDAO = require('database').userProfilesDAO;
 
 // Should we double count browse nodes when adding an item's info to a user profile?
 const DOUBLE_COUNT = false;
@@ -14,39 +15,42 @@ const DOUBLE_COUNT = false;
  */
 function addPurchaseEvent(userid, platform, ASIN) {
 
-    //TODO Get current user profile
-    let profile = {};
+    // Get current user profile
+    return userProfilesDAO.getUserProfile(userid, platform).then(profile => {
 
-    // Get item browse node information from amazon
-    utils.getItemInfo(ASIN).then(item => {
-        let ASIN = item.ASIN && item.ASIN[0];
+        // Get item browse node information from amazon
+        return utils.getItemInfo(ASIN).then(item => {
 
-        // Parse browse node info from response
-        let itemBrowseNodeFreq = utils.collectBrowseNodeFreq(item, DOUBLE_COUNT);
+            let ASIN = item.ASIN && item.ASIN[0];
 
-        // Update user profile browse node frequencies
-        Object.keys(itemBrowseNodeFreq).forEach(key => {
-            // If we haven't seen this item before with this browse node, add the
-            // frequency for this item to the overall frequency for this browse node
-            if ( !(key in profile) ) {
-                // We've never even seen this browse node before, create an entry
-                profile[key] = {
-                    freq: itemBrowseNodeFreq[key].freq,
-                    items: {}
-                };
-            } else if ( !(ASIN in profile[key].items) ) {
-                // We've seen this browse node before but not on this item, increase
-                // the frequency
-                profile[key].freq += itemBrowseNodeFreq[key].freq;
-            }
-            
-            // Even if we've already seen this ASIN, take the chance to update the
-            // the browse node ids associated with this item for this node
-            profile[key].items[ASIN] = itemBrowseNodeFreq[key].BrowseNodeId;
+            // Parse browse node info from response
+            let itemBrowseNodeFreq = utils.collectBrowseNodeFreq(item, DOUBLE_COUNT);
+
+            // Update user profile browse node frequencies
+            Object.keys(itemBrowseNodeFreq).forEach(key => {
+                // If we haven't seen this item before with this browse node, add the
+                // frequency for this item to the overall frequency for this browse node
+                if ( !(key in profile) ) {
+                    // We've never even seen this browse node before, create an entry
+                    profile[key] = {
+                        freq: itemBrowseNodeFreq[key].freq,
+                        items: {}
+                    };
+                } else if ( !(ASIN in profile[key].items) ) {
+                    // We've seen this browse node before but not on this item, increase
+                    // the frequency
+                    profile[key].freq += itemBrowseNodeFreq[key].freq;
+                }
+                
+                // Even if we've already seen this ASIN, take the chance to update the
+                // the browse node ids associated with this item for this node
+                profile[key].items[ASIN] = itemBrowseNodeFreq[key].BrowseNodeId;
+            });
+
+            // Save updated profile
+            console.log(`Profile: ${JSON.stringify(profile, null, 2)}`);
+            return userProfilesDAO.updateUserProfile(userid, platform, profile);
         });
-
-        //TODO Save updated profile
-        console.log(`Profile: ${JSON.stringify(profile, null, 2)}`);
     });
 };
 
